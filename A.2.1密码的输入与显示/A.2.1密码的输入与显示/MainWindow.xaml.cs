df279ecadd82a090ba036487312c796356a1f9ea
yuanjunhuo <17839785874@163.com>
2019-10-22 10:29:36 +0800

    A.2.1密码的输入与显示

---
+++ A.2.1密码的输入与显示/A.2.1密码的输入与显示/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A._2._1密码的输入与显示
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string Password;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+          
+            if (hjyone.IsChecked==true)
+            {
+                hjytwo.Text = hjythree .Password ;
+            }
+
+        }
+    }
+}
